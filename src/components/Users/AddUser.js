8ff1b30de0506e3a928c78262c1a6a374cf0e957
imonweb <contact@imonweb.co.uk>
2022-03-18 14:19:16 +0000

    ErrorModal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,21 +2,30 @@ import React, { useState } from "react"
 
 import Card from "../UI/Card"
 import Button from "../UI/Button"
+import ErrorModal from "../UI/ErrorModal"
 import classes from "./AddUser.module.css"
-// import PropTypes from 'prop-types'
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("")
   const [enteredAge, setEnteredAge] = useState("")
+  const [error, setError] = useState()
 
   const addUserHandler = (event) => {
     event.preventDefault()
     // Validation
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      })
       return
     }
     // enteredAge is a string, to make it a number you have to add + symbol at front.
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Plase enter a valid age (> 0).",
+      })
       return
     }
     console.log(enteredUsername, enteredAge)
@@ -33,8 +42,13 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value)
   }
 
+  const errorHandler = () => {
+    setError(null)
+  }
+
   return (
     <div>
+      {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
